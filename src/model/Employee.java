7f6ceb65559d393b15209d8579b1bf2d245dc9aa
s21dororebe <s21dororebe@venta.lv>
2023-04-04 17:42:21 +0300

    New: classes Person and Employee Edit: Person class - copied from previous work

---
+++ src/model/Employee.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Employee extends Person{
+    private int contractDate;
+    private int contractNumber;
+
+    /*i. contractDate - nepieciešams pārbaudīt, vai līguma datums nav pirms 2023. gada 24.marta (1 punkts);
+    * ii. contractNumber - līguma numurs, kuru nepieciešams izveidot pēc sekojoša formāta <līgumaGads>_<darbinieka vārda
+pirmais burts>_<darbinieka uzvārda pirmais burts>. Piemēram, 2023_K_Š (1 punkts);*/
+}
