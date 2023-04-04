47655a53fdf0ef8542a2370db0e092e8511695a5
s21dororebe <s21dororebe@venta.lv>
2023-04-04 22:37:43 +0300

    Edit: Employee class ToDo: create a logic for constructors

--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,8 +1,37 @@
 package model;
 
+import java.util.*;
+
 public class Employee extends Person{
-    private int contractDate;
-    private int contractNumber;
+    private Date contractDate;
+    private String contractNumber = "";
+
+    public Date getContractDate() {
+        return contractDate;
+    }
+    public void setContractDate(int year, int month, int date) {
+        if(year < 2023 || month < 3 || date < 24) {
+            contractDate = new Date(year, month, date);
+        } else {
+            contractDate = new Date(2023, 3, 24);
+        }
+    }
+
+    public String getContractNumber() {
+        return contractNumber;
+    }
+    public void setContractNumber(String name, String surname, int year) {
+        contractNumber += Integer.toString(year);
+        contractNumber += "_";
+        contractNumber += name.charAt(0);
+        contractNumber += "_";
+        contractNumber += surname.charAt(0);
+    }
+
+    public Employee(int yearOfContract, int month, int date, String name, String surname, int year){
+        setContractDate(year, month, date);
+        setContractNumber(name, surname, year);
+    }
 
     /*i. contractDate - nepieciešams pārbaudīt, vai līguma datums nav pirms 2023. gada 24.marta (1 punkts);
     * ii. contractNumber - līguma numurs, kuru nepieciešams izveidot pēc sekojoša formāta <līgumaGads>_<darbinieka vārda
---
+++ src/services/MainService.java
@@ -0,0 +1,7 @@
+package services;
+
+public class MainService {
+    public static void main(String[] args) {
+
+    }
+}
