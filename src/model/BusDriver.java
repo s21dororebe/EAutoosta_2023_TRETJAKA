434f075827447175d2e9cb6a397ed19483db466a
s21dororebe <s21dororebe@venta.lv>
2023-04-05 15:44:33 +0300

    New and finished: class BusDriver

---
+++ src/model/BusDriver.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class BusDriver extends Employee {
+    private final ArrayList<BusCategory> driveCategories = new ArrayList<>();
+    private int expYears = 0;
+
+    public ArrayList<BusCategory> getDriveCategories() {
+        return driveCategories;
+    }
+
+    public int getExpYears() {
+        return expYears;
+    }
+    public void setExpYears(int inputExpYears) {
+        if(inputExpYears >= 0){
+            expYears = inputExpYears;
+        }
+    }
+
+    public BusDriver() throws Exception {
+    }
+    public BusDriver(String name, String surname, String personCode, int yearOfContract, int monthOfContract, int dateOfContract, int inputExpYears) throws Exception {
+        super(name, surname, personCode, yearOfContract, monthOfContract, dateOfContract);
+        setExpYears(inputExpYears);
+    }
+    public BusDriver(Person personData, int yearOfContract, int monthOfContract, int dateOfContract, int inputExpYears) throws Exception {
+        super(personData, yearOfContract, monthOfContract, dateOfContract);
+        setExpYears(inputExpYears);
+    }
+    public BusDriver(Employee employerData, int inputExpYears) throws Exception {
+        super(employerData.getName(), employerData.getSurname(), employerData.getPersonCode(), employerData.getContractDate().getYear(), employerData.getContractDate().getMonth(), employerData.getContractDate().getDay());
+        setExpYears(inputExpYears);
+    }
+
+    public void addCategory(BusCategory category) throws Exception {
+        for(BusCategory temp : driveCategories){
+            if(category == temp){
+                throw (new Exception("This category already exists"));
+            }
+        }
+        driveCategories.add(category);
+    }
+    public void removeCategory(BusCategory category) throws Exception {
+        boolean indicator = false;
+        for (BusCategory temp : driveCategories) {
+            if (category == temp) {
+                driveCategories.remove(category);
+                indicator = true;
+                break;
+            }
+        }
+        if(!indicator)
+            throw (new Exception("This category does not exist in this array"));
+    }
+
+    @Override
+    public String toString() {
+        if(driveCategories.size() > 0){
+            return super.toString() + ", experience: " + expYears + " years, categories: " + driveCategories;
+        } else {
+            return super.toString() + ", experience: " + expYears + " years";
+        }
+    }
+}
--- src/services/MainService.java
+++ src/services/MainService.java
@@ -1,6 +1,7 @@
 package services;
 
-import model.Date;
+import model.BusCategory;
+import model.BusDriver;
 import model.Employee;
 import model.Person;
 
@@ -11,11 +12,20 @@ public class MainService {
             Person p2 = new Person();
             Employee e1 = new Employee(p1, 2023, 3, 24);
             Employee e2 = new Employee(p2, 2023, 6, 20);
+            BusDriver bd1 = new BusDriver(p1, 2023, 3, 24, 0);
+            BusDriver bd2 = new BusDriver(e2, 5);
 
             System.out.println(p1);
             System.out.println(e1);
             System.out.println(p2);
             System.out.println(e2);
+            System.out.println("Bus Drivers: ");
+            System.out.println(bd1);
+            bd2.addCategory(BusCategory.minibus);
+            bd2.addCategory(BusCategory.schoolbus);
+            System.out.println(bd2);
+            bd2.removeCategory(BusCategory.schoolbus);
+            System.out.println(bd2);
 
         } catch (Exception e){
             System.out.println(e);
